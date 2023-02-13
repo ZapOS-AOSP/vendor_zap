e8fc49ff2bc23f729d1d7e47535336298323d90f
dread <dreadcreator5@gmail.com>
2023-02-13 09:59:40 +0530

    config: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- config/zap_extra.mk
+++ config/zap_extra.mk
@@ -5,6 +5,8 @@ PRODUCT_PACKAGES += \
     RevampedFMRadio
 endif
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 # Use a generic profile based boot image by default
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-profile.txt
