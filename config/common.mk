944dc177b64c456505947e41f65bd3e536567330
dread <dreadcreator5@gmail.com>
2023-02-15 15:23:43 +0530

    vendor: Sort out confusion
    This reverts commit 5e1ff3bbf99f544957fd0e28228a153ba7e8b02c.

--- config/common.mk
+++ config/common.mk
@@ -68,10 +68,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     product_charger_res_images
 
-# Charger
-PRODUCT_PACKAGES += \
-    product_charger_res_images
-
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
     vendor/zap/prebuilt/common/etc/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
