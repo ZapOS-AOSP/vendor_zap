3dbc8921a46ca2306b56b3bcc45f5329d64cce9c
dread <dreadcreator5@gmail.com>
2023-02-18 18:56:36 +0530

    flag: add new flag to make rom BATTERY OPTIMIZED
    set BUILD_BATTERY_OPT := true to add props to optimize battery
    MAY NOT WORK ON ALL DEVICES AND THIS IS NOT TESTED AS OF NOW
    
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- config/zap_extra.mk
+++ config/zap_extra.mk
@@ -15,3 +15,19 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := art/build/boot/boot-image-prof
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     Settings 
+    
+ 
+ # Battery Optimization tweaks - NOT TESTED 
+ # MAY OR MAY NOT WORK FOR YOUR DEVICE
+ 
+ ifeq ($(BUILD_BATTERY_OPT), true)
+PRODUCT_PACKAGES += \
+    PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    	wifi.supplicant_scan_interval=180 \
+    	pm.sleep_mode=1 \
+    	power.saving.mode=1 \
+    	ro.ril.disable.power.collapse=0 \
+    	ro.ril.fast.dormancy.rule=0 \
+    	ro.config.hw_power_saving=1
+endif
+
