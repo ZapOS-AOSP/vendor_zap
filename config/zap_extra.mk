b0e3b44e1dfc1142a87875951af3d1038d356375
dreAd <dreadcreator5@gmail.com>
2023-02-19 16:55:32 +0530

    blurs: add build flag to enable blurs on supported devices
    add TARGET_ENABLE_BLUR:= true to enable blurs on supported devices
    
    Signed-off-by: dreAd <dreadcreator5@gmail.com>

--- config/zap_extra.mk
+++ config/zap_extra.mk
@@ -34,4 +34,15 @@ endif
 # Extra packages
 PRODUCT_PACKAGES += \
     GameSpace
+    
+ 
+# Blurs
+ifeq ($(TARGET_ENABLE_BLUR), true)
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+endif
 
